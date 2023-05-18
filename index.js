86a5bca8c8d2e5c7e70a18a8e0fb9f2875ddbcd4
HoNhatHy <nhathyy.12834@gmail.com>
2023-05-18 20:27:51 +0700

    Nhat Hy : testing

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const updateBotStatus = async (bot, state, bigoUrl) => {
 const runSingleBot = async function (bigoUrl, bot) {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       defaultViewport: null,
       args: ["--start-maximized", "--no-sandbox"],
-      executablePath: "/usr/bin/google-chrome-stable",
+      executablePath:
+        "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
     });
     console.log(`** Start login with ${bot.phoneNum}`);
 
@@ -36,6 +37,7 @@ const runSingleBot = async function (bigoUrl, bot) {
 
 const runMultipleBots = async function (bigoUrl, bots) {
   try {
+    console.log(bigoUrl);
     for (let i = 0; i < bots.length; i++) {
       if (i % 2 == 0) {
         runSingleBot(bigoUrl, bots[i], i);
@@ -49,27 +51,43 @@ const runMultipleBots = async function (bigoUrl, bots) {
 };
 
 const main = async () => {
-  await mongoose.connect(
-    "mongodb+srv://<user>:<password>@bgdb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000",
-    {
-      authMechanism: "SCRAM-SHA-256",
-      dbName: "bgdb",
-      auth: {
-        username: "bgadmin",
-        password: "1204$Honhathy",
-      },
-    }
-  );
+  // await mongoose.connect(
+  //   "mongodb+srv://<user>:<password>@bgdb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000",
+  //   {
+  //     authMechanism: "SCRAM-SHA-256",
+  //     dbName: "bgdb",
+  //     auth: {
+  //       username: "bgadmin",
+  //       password: "1204$Honhathy",
+  //     },
+  //   }
+  // );
 
-  console.log("Connected to mongodb");
+  // console.log("Connected to mongodb");
 
-  const bots = await Bot.find({
-    status: "FREE",
-  }).limit(10);
+  // const bots = await Bot.find({
+  //   status: "FREE",
+  // }).limit(1);
 
-  bots.forEach((x) => updateBotStatus(x, "BUSY", process.env.bigoUrl));
+  // bots.forEach((x) => updateBotStatus(x, "BUSY", process.env.bigoUrl));
+  const bots = [
+    "921627914", // Mỹ Huy
+    "364112810", // Bao Khang
+    "924879261", // Chi Bao
+    "589312506", // Minh Minh
+    "585499059", // Hoang Phuc
+    "567985413", // Hoang Anh
+    "589451236", // Bao Khoa
+    "921627914", // Mỹ Huy
+    "364112810", // Bao Khang
+    "924879261", // Chi Bao
+    "589312506", // Minh Minh
+    "585499059", // Hoang Phuc
+    "567985413", // Hoang Anh
+    "589451236", // Bao Khoa
+  ];
 
-  runMultipleBots(`https://www.bigo.tv/918970883}`, bots);
+  runMultipleBots(`https://www.bigo.tv/918970883`, bots);
 };
 
 main();
